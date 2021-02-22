e58ae6056d485938f65644aa957db8fc0e8fcd0b
aelhanou <elhanouniazeddine00@gmail.com>
2021-02-22 22:11:05 +0100

    brief3

--- js/data.js
+++ js/data.js
@@ -1,74 +1,4 @@
 
-//   var vehicules = [
-    
-//         {
-            
-//             "moto" : {
-//                 "name" : "moto",
-//                 "prix" : 10,
-//                 "carburant" : ["electrique","essence"],
-//                 "type" : [""],
-//                 "img" : "https://freepngimg.com/thumb/motorcycle/1-moto-png-image-motorcycle-png-picture-download.png"
-//             }
-            
-//             ,
-            
-//             "citadine" : {
-//                 "name" : "citadine",
-//                 "prix" : 12,
-//                 "carburant" : ["electrique","hybride","essence","diesel"],
-//                 "type" : ["manuelle"],
-//                 "img" : "https://www.dlm.fr/assets/dyn-img/categories/peugeot208.png"
-//             }
-            
-//             ,
-//             "compact" : {
-//                 "name" : "compact",
-//                 "prix" : 14,
-//                 "carburant" : ["electrique","essence","diesel"],
-//                 "type" : ["manuelle"],
-//                 "img" : "https://cdn.autoportal.com/img/new-cars-gallery/nissan/micra-active/colors/ef8a9e1f95fa942175dc0ad17c2859f3.jpg"
-//             }
-//             ,
-//             "berline" : {
-//                 "name" : "berline",
-//                 "prix" : 20,
-//                 "carburant" : ["diesel","hybride","essence"],
-//                 "type" : ["automatique"],
-//                 "img" : "https://purepng.com/public/uploads/large/purepng.com-white-jaguar-xf-2-carcarvehicletransportjaguar-9615246506373y4og.png"
-//             }
-//             ,
-//             "utilitaire" : {
-//                 "name" : "utilitaire",
-//                 "prix" : 16,
-//                 "carburant" : ["diesel"],
-//                 "type" : ["manuelle"],
-//                 "img" : "https://static.rentacar.fr/images/mediatheque/utilitaire/CAT1B-1.png"
-//             }
-//             ,
-//             "engin" : {
-//                 "name" : "engin",
-//                 "prix" : 900,
-//                 "carburant" : ["diesel","essence"],
-//                 "type" : ["automatique"],
-//                 "img" : "http://s7d2.scene7.com/is/image/Caterpillar/C10087683"
-//             }    
-//             ,
-//             "camion" : {
-//                 "name" : "camion",
-//                 "prix" : 250,
-//                 "carburant" : ["diesel"],
-//                 "type" : ["automatique"],
-//                 "img" : "/images/truck.png"
-//             }
-            
-        
-//         }
-
-//     ]
-
-
-
     var vehicules1 = [[{
 
         "info" : {
@@ -139,6 +69,6 @@
             "perCarburant" :[,,,21],
             "type" : ["automatique"],
             "porcentage": [19],
-            "img" : "/images/truck.png"
+            "img" : "images/truck.png"
         }
     }]]
--- js/newJs.js
+++ js/newJs.js
@@ -5,10 +5,19 @@ const manAuto = document.querySelectorAll(".hi1");
 var jours = document.querySelector("#jour");
 var output = document.getElementById("output");
 var i, j, total, jour, btvPrix = 0, per, g, prix;
+console.log(gen6[7]);
 function carburant(e) {
     g = e;
-
     i = 0;
+    for(j = 0;j < gen6.length;j++)
+    {
+        gen6[j].style.border = "none";
+    }
+    gen6[1].addEventListener("click",function(){
+        gen6[1].style.border = "1px solid red";
+        gen6[0].style.border = "none";
+    })
+    imgActiv(e);
     for (var j = 0; j < type.length; j++) {
         type[j].style.display = "block";
     }
@@ -33,6 +42,10 @@ function carburant(e) {
             manAuto[j].style.display = "none";
         }
     }
+    
+}
+function imgActiv(t) {
+    gen6[t].style.border = "1px solid red";
 }
 function btvCalcul(n) {
     per = vehicules1[g][0].info.perCarburant[n];
@@ -45,5 +58,4 @@ function calcul() {
     var jr = jours.value;
     total = ((per + btvPrix) * (jr * prix)) / 100 + (jr * prix);
     output.innerHTML = "Total : " + total + " €";
-    
 }
